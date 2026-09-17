b222dfa0242032a41be2a6f145368afa9b0e8de2
devjimenez27 <dev.jimenez27@gmail.com>
2026-09-17 20:41:24 +0800

    elective

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/Elective",
-  assetPrefix: "/Elective/",
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
